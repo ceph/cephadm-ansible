e4cb989d11a174030ae3f2313650dd91c9fbb9d0
Teoman ONAY <tonay@ibm.com>
2023-12-04 15:40:51 +0100

    ansible: bump version to 2.12 to 2.15
    - Ansible 2.9 is not supported since May 2022
    - requirements versions filter removed to use latest versions
    
    Signed-off-by: Teoman ONAY <tonay@ibm.com>

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -4,7 +4,7 @@ import pytest
 def node(host, request):
     ansible_vars = host.ansible.get_variables()
 
-    if request.node.get_closest_marker("no_client") and ansible_vars['group_names'] == ['clients']:
+    if request.node.get_closest_marker("no_client") and 'clients' in ansible_vars['group_names']:
         pytest.skip("Not a valid test for client nodes")
 
     if request.node.get_closest_marker("client") and 'clients' not in ansible_vars['group_names']:
