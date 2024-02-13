ceph/cephadm-ansible

commit 2645b6ad95be2ed787a51d43739deb8a7e098143

bootstrap: make the module idempotent
the module fails when it's run against a cluster already deployed.
this commits makes it skip and no throw an error if a cluster is
already running.

Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

library/cephadm_bootstrap.py
@@ -22,7 +22,7 @@ try:
 except ImportError:
     from module_utils.ceph_common import exit_module
 import datetime
-
+import os
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
@@ -193,6 +193,37 @@ def main() -> None:
     startd = datetime.datetime.now()
 
     cmd = ['cephadm']
+    data_dir = '/var/lib/ceph'
+    ceph_conf = 'ceph.conf'
+    ceph_keyring = 'ceph.client.admin.keyring'
+    ceph_pubkey = 'ceph.pub'
+
+    if fsid:
+        if os.path.exists(os.path.join(data_dir, fsid)):
+            out = 'A cluster with fsid {} is already deployed.'.format(fsid)
+            exit_module(
+                rc=0,
+                startd=startd,
+                module=module,
+                cmd=cmd,
+                out=out,
+                changed=False
+            )
+
+    for f in [ceph_conf,
+              ceph_keyring,
+              ceph_pubkey]:
+        if not allow_overwrite:
+            if os.path.exists(os.path.join(data_dir, f)):
+                out = '{} already exists, skipping.'
+                exit_module(
+                    rc=0,
+                    startd=startd,
+                    module=module,
+                    cmd=cmd,
+                    out=out,
+                    changed=False
+                )
 
     if docker:
         cmd.append('--docker')

module_utils/ceph_common.py
@@ -19,8 +19,10 @@ def build_cmd(cli_binary: bool,
 
 
 def exit_module(module: "AnsibleModule",
-                out: str, rc: int, cmd: List[str],
-                err: str, startd: datetime.datetime,
+                rc: int, cmd: List[str],
+                startd: datetime.datetime,
+                out: str = '',
+                err: str = '',
                 changed: bool = False,
                 diff: Dict[str, str] = dict(before="", after="")) -> None:
     endd = datetime.datetime.now()

commit a31eef878860a1bde01c5d6d1e06f3bb61cb200a

library: fix error message in cephadm_bootstrap module
2645b6ad95be2ed787a51d43739deb8a7e098143 introduced a cosmetic issue.

That message was intended to print the path using f-strings.
This commit addresses this.

Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

library/cephadm_bootstrap.py
@@ -270,8 +270,9 @@ def run_module() -> None:
               ceph_keyring,
               ceph_pubkey]:
         if not allow_overwrite:
-            if os.path.exists(os.path.join(data_dir, f)):
-                out = '{} already exists, skipping.'
+            path: str = os.path.join(data_dir, f)
+            if os.path.exists(path):
+                out = f'{path} already exists, skipping.'
                 exit_module(
                     rc=0,
                     startd=startd,